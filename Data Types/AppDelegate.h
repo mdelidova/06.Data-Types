e883ed129325077c73f81f204cc4c2bc97eeafa5
Maryna Delidova <marynadelidova@marynas-macbook-pro.local>
2018-03-22 18:29:58 +0200

    Initial Commit

---
+++ Data Types/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Data Types
+//
+//  Created by Maryna Delidova on 3/22/18.
+//  Copyright © 2018 Maryna Delidova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Data Types/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Data Types
+//
+//  Created by Maryna Delidova on 3/22/18.
+//  Copyright © 2018 Maryna Delidova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
